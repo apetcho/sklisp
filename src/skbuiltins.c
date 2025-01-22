1f1214ad0dabb5b009676e741d441e59bb383f9f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-01-22 23:30:51 +0100

    Implement _arithm()

---
+++ src/skbuiltins.c
@@ -0,0 +1,415 @@
+#include "sklisp.h"
+
+static Self _fn_num_eq(Self self);
+
+// -*-
+static Self _fn_docstr(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_apply(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_and(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_or(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_cons(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_quote(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_lambda(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_defun(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_defmacro(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_cdr(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_car(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_list(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_if(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_not(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_cond(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_progn(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_let(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_special_while(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_eval(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_eq(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_eql(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_hash(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_print(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_println(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_set(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_value(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_symbol_name(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_str_concat(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_str_len(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_nullp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_callablep(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_listp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_symbolp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_numberp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_stringp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_integerp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_floatp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_vectorp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_load(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_parse(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_throw(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_catch(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_vec_set(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_vec_get(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_vec_len(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_new_vec(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_vec_concat(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_vec_slice(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_refcount(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_eval_depth(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_max_eval_depth(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_exit(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_list_len(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_len(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_typeof(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+void skl_init_builtins(void){
+    SKL_SYMBOL_SET(skl_new_symbol("docstr"), skl_new_fun(_fn_docstr));
+    SKL_SYMBOL_SET(skl_new_symbol("apply"), skl_new_fun(_fn_apply));
+    SKL_SYMBOL_SET(skl_new_symbol("and"), skl_new_special(_fn_special_and));
+    SKL_SYMBOL_SET(skl_new_symbol("or"), skl_new_special(_fn_special_or));
+    SKL_SYMBOL_SET(skl_new_symbol("quote"), skl_new_special(_fn_special_quote));
+    SKL_SYMBOL_SET(skl_new_symbol("lambda"), skl_new_special(_fn_special_lambda));
+    SKL_SYMBOL_SET(skl_new_symbol("defun"), skl_new_special(_fn_special_defun));
+    SKL_SYMBOL_SET(skl_new_symbol("doefmacro"), skl_new_special(_fn_special_defmacro));
+    SKL_SYMBOL_SET(skl_new_symbol("car"), skl_new_fun(_fn_car));
+    SKL_SYMBOL_SET(skl_new_symbol("cdr"), skl_new_fun(_fn_cdr));
+    SKL_SYMBOL_SET(skl_new_symbol("list"), skl_new_fun(_fn_list));
+    SKL_SYMBOL_SET(skl_new_symbol("if"), skl_new_special(_fn_special_if));
+    SKL_SYMBOL_SET(skl_new_symbol("not"), skl_new_fun(_fn_not));
+    SKL_SYMBOL_SET(skl_new_symbol("progn"), skl_new_special(_fn_special_progn));
+    SKL_SYMBOL_SET(skl_new_symbol("let"), skl_new_special(_fn_special_let));
+    SKL_SYMBOL_SET(skl_new_symbol("while"), skl_new_special(_fn_special_while));
+    SKL_SYMBOL_SET(skl_new_symbol("eval"), skl_new_fun(_fn_eval));
+    SKL_SYMBOL_SET(skl_new_symbol("print"), skl_new_fun(_fn_print));
+    SKL_SYMBOL_SET(skl_new_symbol("println"), skl_new_fun(_fn_println));
+    SKL_SYMBOL_SET(skl_new_symbol("cons"), skl_new_fun(_fn_cons));
+
+    SKL_SYMBOL_SET(skl_new_symbol("set"), skl_new_fun(_fn_set));
+    SKL_SYMBOL_SET(skl_new_symbol("value"), skl_new_fun(_fn_value));
+    SKL_SYMBOL_SET(skl_new_symbol("symbol-name"), skl_new_fun(_fn_symbol_name));
+
+    SKL_SYMBOL_SET(skl_new_symbol("string-concat"), skl_new_fun(_fn_str_concat));
+    SKL_SYMBOL_SET(skl_new_symbol("string-len"), skl_new_fun(_fn_str_len));
+
+    SKL_SYMBOL_SET(skl_new_symbol("eq"), skl_new_fun(_fn_eq));
+    SKL_SYMBOL_SET(skl_new_symbol("eql"), skl_new_fun(_fn_eql));
+    SKL_SYMBOL_SET(skl_new_symbol("hash"), skl_new_fun(_fn_hash));
+
+    SKL_SYMBOL_SET(skl_new_symbol("null?"), skl_new_fun(_fn_nullp));
+    SKL_SYMBOL_SET(skl_new_symbol("nullp"), skl_new_fun(_fn_nullp));
+    SKL_SYMBOL_SET(skl_new_symbol("list?"), skl_new_fun(_fn_listp));
+    SKL_SYMBOL_SET(skl_new_symbol("listp"), skl_new_fun(_fn_listp));
+    SKL_SYMBOL_SET(skl_new_symbol("callable?"), skl_new_fun(_fn_callablep));
+    SKL_SYMBOL_SET(skl_new_symbol("callablep"), skl_new_fun(_fn_callablep));
+    SKL_SYMBOL_SET(skl_new_symbol("symbol?"), skl_new_fun(_fn_symbolp));
+    SKL_SYMBOL_SET(skl_new_symbol("symbolp"), skl_new_fun(_fn_symbolp));
+    SKL_SYMBOL_SET(skl_new_symbol("string?"), skl_new_fun(_fn_stringp));
+    SKL_SYMBOL_SET(skl_new_symbol("stringp"), skl_new_fun(_fn_stringp));
+    SKL_SYMBOL_SET(skl_new_symbol("number?"), skl_new_fun(_fn_numberp));
+    SKL_SYMBOL_SET(skl_new_symbol("numberp"), skl_new_fun(_fn_numberp));
+    SKL_SYMBOL_SET(skl_new_symbol("integer?"), skl_new_fun(_fn_integerp));
+    SKL_SYMBOL_SET(skl_new_symbol("integerp"), skl_new_fun(_fn_integerp));
+    SKL_SYMBOL_SET(skl_new_symbol("float?"), skl_new_fun(_fn_floatp));
+    SKL_SYMBOL_SET(skl_new_symbol("floatp"), skl_new_fun(_fn_floatp));
+    SKL_SYMBOL_SET(skl_new_symbol("vector?"), skl_new_fun(_fn_vectorp));
+    SKL_SYMBOL_SET(skl_new_symbol("vectorp"), skl_new_fun(_fn_vectorp));
+
+    SKL_SYMBOL_SET(skl_new_symbol("load"), skl_new_fun(_fn_load));
+    SKL_SYMBOL_SET(skl_new_symbol("parse"), skl_new_fun(_fn_parse));
+
+    SKL_SYMBOL_SET(skl_new_symbol("throw"), skl_new_fun(_fn_throw));
+    SKL_SYMBOL_SET(skl_new_symbol("catch"), skl_new_fun(_fn_catch));
+
+    SKL_SYMBOL_SET(skl_new_symbol("vector-set"), skl_new_fun(_fn_vec_set));
+    SKL_SYMBOL_SET(skl_new_symbol("vector-get"), skl_new_fun(_fn_vec_get));
+    SKL_SYMBOL_SET(skl_new_symbol("vector-len"), skl_new_fun(_fn_vec_len));
+    SKL_SYMBOL_SET(skl_new_symbol("new-vector"), skl_new_fun(_fn_new_vec));
+    SKL_SYMBOL_SET(skl_new_symbol("vector-concat"), skl_new_fun(_fn_vec_concat));
+    SKL_SYMBOL_SET(skl_new_symbol("vector-slice"), skl_new_fun(_fn_vec_slice));
+
+    SKL_SYMBOL_SET(skl_new_symbol("refcount"), skl_new_fun(_fn_refcount));
+    SKL_SYMBOL_SET(skl_new_symbol("eval-depth"), skl_new_fun(_fn_eval_depth));
+    SKL_SYMBOL_SET(skl_new_symbol("max-eval-depth"), skl_new_fun(_fn_max_eval_depth));
+
+    SKL_SYMBOL_SET(skl_new_symbol("exit"), skl_new_fun(_fn_exit));
+
+    SKL_SYMBOL_SET(skl_new_symbol("list-len"), skl_new_fun(_fn_list_len));
+    SKL_SYMBOL_SET(skl_new_symbol("len"), skl_new_fun(_fn_len));
+    SKL_SYMBOL_SET(skl_new_symbol("typeof"), skl_new_fun(_fn_typeof));
+
+    SKL_SYMBOL_SET(skl_new_symbol("new-channel"), skl_new_fun(skl_new_channel));
+    SKL_SYMBOL_SET(skl_new_symbol("channel-recv"), skl_new_fun(skl_channel_receive));
+    SKL_SYMBOL_SET(skl_new_symbol("channel-send"), skl_new_fun(skl_channel_send));
+
+}
---
+++ src/skmath.c
@@ -0,0 +1,509 @@
+#include "sklisp.h"
+#include<math.h>
+#include<tgmath.h>
+#include<limits.h>
+#include<float.h>
+
+
+typedef enum { ADD, SUB, MUL, DIV } ArithOp;
+typedef enum { EQ, LT, LE, GT, GE } CmpOp;
+
+// -*-
+static Self _arithm(ArithOp op, Self self){
+    if(op==DIV){
+        SKL_EXPECT_MIN_LEN(self, 2, skl_new_symbol("div"));
+    }
+    bool imode = true;
+    Self ival = sklisp.Nil;
+    Self fval = sklisp.Nil;
+    Self val = skl_new_float(0.0);
+    switch(op){
+    case SUB:
+    case ADD:
+        ival = skl_new_integer(0);
+        fval = skl_new_float(0.0);
+        break;
+    case MUL:
+    case DIV:
+        ival = skl_new_integer(1);
+        fval = skl_new_float(1.0);
+        break;
+    }
+    if(op==SUB || op == DIV){
+        Self num = SKL_CAR(self);
+        if(SKL_IS_FLOAT(num)){
+            imode = false;
+            mpf_set(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+        }else if(SKL_IS_INTEGER(num)){
+            mpf_set_z(SKL_DEREF_FLOAT(ival), SKL_DEREF_INTEGER(num));
+            mpz_set(SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(num));
+        }else{
+            skl_delete(ival);
+            skl_delete(fval);
+            skl_delete(val);
+            SKL_THROW(sklisp.TypeError, SKL_INC_RC(num));
+        }
+
+        if(op==SUB && SKL_CDR(self) == sklisp.Nil){
+            if(imode){
+                skl_delete(fval);
+                skl_delete(val);
+                mpz_neg(SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(ival));
+                return ival;
+            }else{
+                skl_delete(ival);
+                skl_delete(val);
+                mpf_neg(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval));
+                return fval;
+            }
+        }
+
+        self = SKL_CDR(self);
+    }
+
+    while(self != sklisp.Nil){
+        Self num = SKL_CAR(self);
+        if(!SKL_IS_NUMBER(num)){
+            skl_delete(ival);
+            skl_delete(fval);
+            skl_delete(val);
+            SKL_THROW(sklisp.TypeError, SKL_INC_RC(num));
+        }
+
+        // check divide by zero
+        if(op == DIV){
+            double x = 0.0;
+            if(SKL_IS_FLOAT(num)){ x = skl_to_float(num); }
+            else{ x = skl_to_integer(num); }
+            if(x == 0){
+                skl_delete(ival);
+                skl_delete(fval);
+                skl_delete(val);
+                SKL_THROW(skl_new_symbol("divide-by-zero"), SKL_INC_RC(num));
+            }
+        }
+        if(imode){
+            if(SKL_IS_FLOAT(num)){
+                imode = false;
+                mpf_set_z(SKL_DEREF_FLOAT(fval), SKL_DEREF_INTEGER(ival));
+                // fall-though to int-mode (i.e imode)
+            }else if(SKL_IS_INTEGER(num)){
+                switch(op){
+                case ADD:
+                    mpz_add(SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(SKL_CAR(self)));
+                    break;
+                case MUL:
+                    mpz_mul(SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(SKL_CAR(self)));
+                    break;
+                case SUB:
+                    mpz_sub(SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(SKL_CAR(self)));
+                    break;
+                case DIV:
+                    mpz_div(SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(ival), SKL_DEREF_INTEGER(SKL_CAR(self)));
+                    break;
+                }
+            }
+        }
+        if(!imode){
+            if(SKL_IS_FLOAT(num)){
+                switch(op){
+                case ADD:
+                    mpf_add(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                case MUL:
+                    mpf_mul(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                case SUB:
+                    mpf_sub(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                case DIV:
+                    mpf_div(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                }
+            }else if(SKL_IS_INTEGER(num)){
+                // convert to float and add.
+                mpf_set_z(SKL_DEREF_FLOAT(val), SKL_DEREF_INTEGER(num));
+                switch(op){
+                case ADD:
+                    mpf_add(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                case MUL:
+                    mpf_mul(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                case SUB:
+                    mpf_sub(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                case DIV:
+                    mpf_div(SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(fval), SKL_DEREF_FLOAT(num));
+                    break;
+                }
+            }
+        }
+        self = SKL_CDR(self);
+    }
+    skl_delete(val);
+
+    // detroy whater went unused
+    if(imode){
+        skl_delete(fval);
+        return ival;
+    }
+    skl_delete(ival);
+    return fval;
+}
+
+// -*-
+static Self _fn_add(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_mul(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_sub(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_div(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _num_cmp(CmpOp op, Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_num_eq(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_num_lt(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_num_le(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_num_gt(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_num_ge(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_mod(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_abs(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_max(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_min(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_isnan(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_isinf(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_isfinite(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_exp(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_exp2(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_expm1(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_log(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_log10(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_log2(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_log1p(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_pow(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_sqrt(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_cbrt(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_hypot(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_sin(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_cos(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_tan(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_asin(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_acos(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_atan(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_atan2(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_sinh(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_cosh(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_tanh(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_asinh(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_acosh(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_atanh(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_erf(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_erfc(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_tgamma(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_lgamma(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_ceil(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_floor(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_trunc(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+static Self _fn_round(Self self){
+    //! @todo
+    return NULL;
+}
+
+// -*-
+void skl_init_math(void){
+    //! @todo
+    SKL_SYMBOL_SET(skl_new_symbol("PI"), skl_new_float(3.141592653589793));
+    SKL_SYMBOL_SET(skl_new_symbol("E"), skl_new_float(2.718281828459045));
+    SKL_SYMBOL_SET(skl_new_symbol("Int::MIN"), skl_new_integer(LONG_MIN));
+    SKL_SYMBOL_SET(skl_new_symbol("Int::MAX"), skl_new_integer(LONG_MAX));
+    SKL_SYMBOL_SET(skl_new_symbol("Float::MIN"), skl_new_float(DBL_MIN));
+    SKL_SYMBOL_SET(skl_new_symbol("Float::MAX"), skl_new_float(DBL_MAX));
+    SKL_SYMBOL_SET(skl_new_symbol("Float::EPSILON"), skl_new_float(DBL_EPSILON));
+
+    SKL_SYMBOL_SET(skl_new_symbol("+"), skl_new_fun(_fn_add));
+    SKL_SYMBOL_SET(skl_new_symbol("-"), skl_new_fun(_fn_sub));
+    SKL_SYMBOL_SET(skl_new_symbol("*"), skl_new_fun(_fn_mul));
+    SKL_SYMBOL_SET(skl_new_symbol("/"), skl_new_fun(_fn_div));
+    SKL_SYMBOL_SET(skl_new_symbol("%"), skl_new_fun(_fn_mod));
+    SKL_SYMBOL_SET(skl_new_symbol("="), skl_new_fun(_fn_num_eq));
+    SKL_SYMBOL_SET(skl_new_symbol("<"), skl_new_fun(_fn_num_lt));
+    SKL_SYMBOL_SET(skl_new_symbol(">"), skl_new_fun(_fn_num_gt));
+    SKL_SYMBOL_SET(skl_new_symbol("<="), skl_new_fun(_fn_num_le));
+    SKL_SYMBOL_SET(skl_new_symbol(">="), skl_new_fun(_fn_num_ge));
+
+    SKL_SYMBOL_SET(skl_new_symbol("abs"), skl_new_fun(_fn_abs));
+    SKL_SYMBOL_SET(skl_new_symbol("max"), skl_new_fun(_fn_max));
+    SKL_SYMBOL_SET(skl_new_symbol("min"), skl_new_fun(_fn_min));
+    SKL_SYMBOL_SET(skl_new_symbol("nan?"), skl_new_fun(_fn_isnan));
+    SKL_SYMBOL_SET(skl_new_symbol("inf?"), skl_new_fun(_fn_isinf));
+    SKL_SYMBOL_SET(skl_new_symbol("finite?"), skl_new_fun(_fn_isfinite));
+    SKL_SYMBOL_SET(skl_new_symbol("exp"), skl_new_fun(_fn_exp));
+    SKL_SYMBOL_SET(skl_new_symbol("exp2"), skl_new_fun(_fn_exp2));
+    SKL_SYMBOL_SET(skl_new_symbol("expm1"), skl_new_fun(_fn_expm1));
+    SKL_SYMBOL_SET(skl_new_symbol("log"), skl_new_fun(_fn_log));
+    SKL_SYMBOL_SET(skl_new_symbol("ln"), skl_new_fun(_fn_log));
+    SKL_SYMBOL_SET(skl_new_symbol("log10"), skl_new_fun(_fn_log10));
+    SKL_SYMBOL_SET(skl_new_symbol("log2"), skl_new_fun(_fn_log2));
+    SKL_SYMBOL_SET(skl_new_symbol("pow"), skl_new_fun(_fn_pow));
+    SKL_SYMBOL_SET(skl_new_symbol("sqrt"), skl_new_fun(_fn_sqrt));
+    SKL_SYMBOL_SET(skl_new_symbol("cbrt"), skl_new_fun(_fn_cbrt));
+    SKL_SYMBOL_SET(skl_new_symbol("hypot"), skl_new_fun(_fn_hypot));
+    SKL_SYMBOL_SET(skl_new_symbol("sin"), skl_new_fun(_fn_sin));
+    SKL_SYMBOL_SET(skl_new_symbol("cos"), skl_new_fun(_fn_cos));
+    SKL_SYMBOL_SET(skl_new_symbol("tan"), skl_new_fun(_fn_tan));
+    SKL_SYMBOL_SET(skl_new_symbol("asin"), skl_new_fun(_fn_asin));
+    SKL_SYMBOL_SET(skl_new_symbol("acos"), skl_new_fun(_fn_acos));
+    SKL_SYMBOL_SET(skl_new_symbol("atan"), skl_new_fun(_fn_atan));
+    SKL_SYMBOL_SET(skl_new_symbol("atan2"), skl_new_fun(_fn_atan2));
+    SKL_SYMBOL_SET(skl_new_symbol("sinh"), skl_new_fun(_fn_sinh));
+    SKL_SYMBOL_SET(skl_new_symbol("cosh"), skl_new_fun(_fn_cosh));
+    SKL_SYMBOL_SET(skl_new_symbol("tanh"), skl_new_fun(_fn_tanh));
+    SKL_SYMBOL_SET(skl_new_symbol("asinh"), skl_new_fun(_fn_asinh));
+    SKL_SYMBOL_SET(skl_new_symbol("acosh"), skl_new_fun(_fn_acosh));
+    SKL_SYMBOL_SET(skl_new_symbol("atanh"), skl_new_fun(_fn_atanh));
+    SKL_SYMBOL_SET(skl_new_symbol("erf"), skl_new_fun(_fn_erf));
+    SKL_SYMBOL_SET(skl_new_symbol("erfc"), skl_new_fun(_fn_erfc));
+    SKL_SYMBOL_SET(skl_new_symbol("tgamma"), skl_new_fun(_fn_tgamma));
+    SKL_SYMBOL_SET(skl_new_symbol("lgamma"), skl_new_fun(_fn_lgamma));
+    SKL_SYMBOL_SET(skl_new_symbol("ceil"), skl_new_fun(_fn_ceil));
+    SKL_SYMBOL_SET(skl_new_symbol("floor"), skl_new_fun(_fn_floor));
+    SKL_SYMBOL_SET(skl_new_symbol("trunc"), skl_new_fun(_fn_trunc));
+    SKL_SYMBOL_SET(skl_new_symbol("round"), skl_new_fun(_fn_round));
+}
