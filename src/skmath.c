3648f21dc0ed721ce9e47acf9ca3c40c6796ce63
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-01-23 02:21:04 +0100

    Implement _fn_log1p()

--- src/skmath.c
+++ src/skmath.c
@@ -541,8 +541,15 @@ static Self _fn_log2(Self self){
 
 // -*-
 static Self _fn_log1p(Self self){
-    //! @todo
-    return NULL;
+    SKL_DOC("Compute natural (base-e) logarithm of 1 plus given number");
+    SKL_EXPECT_LEN(self, 1, skl_new_symbol("ln(1+x)"));
+    self = SKL_CAR(self);
+    if(!SKL_IS_NUMBER(self)){
+        SKL_THROW(sklisp.TypeError, self);
+    }
+    double x = skl_to_float(self);
+    if(x <= 0){ SKL_THROW(sklisp.ValueError, self); }
+    return skl_new_integer(log1p(x));
 }
 
 // -*-
