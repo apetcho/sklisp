ddc32551911c7ce36c8a743a63284fe60402b1c4
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-01-23 03:05:16 +0100

    Implement _fn_ceil()

--- src/skmath.c
+++ src/skmath.c
@@ -821,8 +821,14 @@ static Self _fn_lgamma(Self self){
 
 // -*-
 static Self _fn_ceil(Self self){
-    //! @todo
-    return NULL;
+    SKL_DOC("Compute smallest integer not less than given value");
+    SKL_EXPECT_LEN(self, 1, skl_new_symbol("*ceil*"));
+    self = SKL_CAR(self);
+    if(!SKL_IS_NUMBER(self)){
+        SKL_THROW(sklisp.TypeError, self);
+    }
+    double x = skl_to_float(self);
+    return skl_new_integer(ceil(x));
 }
 
 // -*-
